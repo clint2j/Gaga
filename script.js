030a51343169ee7b30f90a2528222ff2a568fcd2
clint2j <94955448+clint2j@users.noreply.github.com>
2021-11-24 03:04:29 -0600

    Added files

---
+++ script.js
@@ -0,0 +1,11 @@
+function bruh() {
+  document.getElementById("initial").style.display = "none";
+  document.getElementById("login").style.display = "block";
+}
+function ubruh() {
+  document.getElementById("initial").style.display = "block";
+  document.getElementById("login").style.display = "none";
+}
+function forgo() {
+  alert("If you have any login issues (like forgotten passwords or user names, or glitches), email the site at cj@gagaball.xyz (preferably from an @tamu.edu account for verification)");
+}
